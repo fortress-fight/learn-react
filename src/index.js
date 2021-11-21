ade9a6445d9d01815953bd7c13cf066deb8277ab
ff-stone <dreamersfly@163.com>
2021-11-22 01:35:50 +0800

    update(env): HOME-MACBOOK 配置环境

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Description: 初始项目入口
  * @Author: F-Stone
  * @Date: 2021-11-16 10:48:47
- * @LastEditTime: 2021-11-19 19:32:56
+ * @LastEditTime: 2021-11-22 01:22:11
  * @LastEditors: F-Stone
  */
 import "./learn-group-04/project-06";
---
+++ src/learn-group-05/project-01.js
@@ -0,0 +1,19 @@
+/*
+ * @Description: 
+ * @Author: F-Stone
+ * @Date: 2021-11-21 23:47:31
+ * @LastEditTime: 2021-11-22 01:30:04
+ * @LastEditors: F-Stone
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App () {
+    return (
+        <div>
+            <span>测试</span>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
